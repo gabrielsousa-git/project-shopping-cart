74324b0eed0313281caab59f88c06c7f7c7b871a
Gabriel Sousa <gabrielsousa224.gs@gmail.com>
2022-07-11 18:42:54 -0300

    Iniciando o primeiro projeto do módulo de Front end VQV!

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,16 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async (productName) => {
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${productName}`;
+  if (!productName) return ('You must provide an url');
+
+  try {
+    const products = await fetch(url)
+    .then((response) => response.json())
+    .then((data) => data);
+    
+    return products;
+  } catch (error) {
+    throw new Error('You must provide an url');
+  }
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,17 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('é uma função', () => {
+    expect(typeof(fetchProducts)).toEqual('function');
+  });
+  it('é chamada quando passado o argumento "computador"', () => {
+    fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledTimes(1);
+  });
+  it('retorna um erro com a mensagem: "You must provide an url" se chamada sem argumento', async () => {
+    expect( await fetchProducts()).toEqual(('You must provide an url'));
+  });
+  it('retorna todos os produtos', async () => {
+    expect( await fetchProducts('computador')).toEqual((computadorSearch));
+  });
 });
