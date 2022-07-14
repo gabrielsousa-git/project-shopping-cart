8eb0b88155177d8139d3562dae9fa01c1b169a92
Gabriel Sousa <gabrielsousa224.gs@gmail.com>
2022-07-14 19:51:15 -0300

    Requisito 7 concluído

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -4,6 +4,15 @@ const getSavedCartItems = require('../helpers/getSavedCartItems');
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('é uma função', () => {
+    expect(typeof(getSavedCartItems)).toEqual('function');
+  });
+  it('ao ser executada, o método localStorage.getItem é chamado', () => {
+    getSavedCartItems();
+    expect(localStorage.getItem).toHaveBeenCalledTimes(1);
+  });
+  it('ao ser executada, o método localStorage.getItem é chamado com o "cartItems" como parâmetro', () => {
+    getSavedCartItems('cartItems');
+    expect(localStorage.getItem).toHaveBeenCalledWith('cartItems');
+  });
 });
